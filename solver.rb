4e0744c52a1f37ed7362346f94a82437f761af70
Olanrewaju Oduola <111298026+David-Lanzz@users.noreply.github.com>
2023-06-03 06:23:13 +0100

    Fix linter errors

--- solver.rb
+++ solver.rb
@@ -1,29 +1,27 @@
 class Solver
+  def factorial(num)
+    return "Cannot find factorial of negative number #{num}" if num.negative?
 
-def factorial(num)
-  if num < 0
-    return "Cannot find factorial of negative number #{num}"
+    lim = 1
+    (1..num).each do |n|
+      lim *= n
+    end
+    lim
   end
-  lim = 1
-(1..num).each{|n|
-  lim *= n
-  }
- lim
-end
 
-def reverse(word)
- word.reverse!
-end
+  def reverse(word)
+    word.reverse!
+  end
 
-def fizzbuzz(num)
- if num % 5 == 0 && num % 3 == 0
-  "fizzbuzz"
- elsif num % 5 == 0
-  "buzz"
- elsif num % 3 == 0
-  "fizz"
- else
-  num
- end
-end
+  def fizzbuzz(num)
+    if (num % 5).zero? && (num % 3).zero?
+      'fizzbuzz'
+    elsif (num % 5).zero?
+      'buzz'
+    elsif (num % 3).zero?
+      'fizz'
+    else
+      num
+    end
+  end
 end
