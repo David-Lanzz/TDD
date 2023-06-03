2d770ad241d2755e97e597fb8e5cbc9b50fab126
Olanrewaju Oduola <111298026+David-Lanzz@users.noreply.github.com>
2023-06-03 05:40:48 +0100

    Write test for factorial

--- solvertest.rb
+++ solvertest.rb
@@ -1,5 +1,9 @@
 require_relative "solver.rb"
 
 describe Solver do
+   solver = Solver.new
 
+   it "Should expect factorial to return the factorial of provided integer" do
+    expect(Solver.factorial(3).to eq(6))
+   end
 end
