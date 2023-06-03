4e3f7778b4c8524f977a29a3a47fa5d63b70657f
Olanrewaju Oduola <111298026+David-Lanzz@users.noreply.github.com>
2023-06-03 05:49:20 +0100

    Solve factorial function

--- solver.rb
+++ solver.rb
@@ -1,3 +1,11 @@
 class Solver
 
+def factorial(num)
+  lim = 1
+(1..num).each{|n|
+  lim *= n
+  }
+ lim
+end
+
 end
--- solvertest.rb
+++ solvertest.rb
@@ -2,8 +2,7 @@ require_relative "solver.rb"
 
 describe Solver do
    solver = Solver.new
-
    it "Should expect factorial to return the factorial of provided integer" do
-    expect(Solver.factorial(3).to eq(6))
+    expect(solver.factorial(5)).to eq(120)
    end
 end
